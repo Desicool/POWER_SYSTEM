946a835d261882d7bdfd7f7ca4fe75b6cad0fc31
Desicool <609936947@qq.com>
2018-12-20 19:11:46 +0800

    Revert "添加项目文件。"
    This reverts commit 5f7c3e64ed153494cca937a2f364522b6ccde248.

--- POWER_SYSTEM/characterDAO.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Connection.h"
-using namespace std;
-namespace characterDAO {
-	sql::ResultSet* get_character(string ID);
-	int get_privilege(string characterID);
-}
--- POWER_SYSTEM/userDAO.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Connection.h"
-#include <stack>
-using namespace std;
-namespace userDAO{
-	string login_check(string username, string pwd);
-	sql::ResultSet* get_user_info(string ID);
-	int regist(string _name,string _pwd,string _phone,string _address,string _email,string _identity);
-}
